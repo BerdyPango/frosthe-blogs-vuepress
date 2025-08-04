5eed5016f5d3b8cf49a24385105b7c320747cf74
Frost He <jia-xun.he@ubisoft.com>
2025-08-04 13:28:27 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,7 +6,7 @@ import { viteBundler } from '@vuepress/bundler-vite'
 export default defineUserConfig({
   lang: 'en-US',
 
-  title: 'FrostHe.Handbook',
+  title: 'FrostHe.TechBook',
   description: 'FrostHe\'s Personal blog site',
 
   theme: defaultTheme({
